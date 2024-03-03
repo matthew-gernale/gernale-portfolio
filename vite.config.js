87c8d4b74a413e3dedeef606b99d91d0e2eeae1e
John Matthew T. Gernale <151718007+matthew-gernale@users.noreply.github.com>
2024-03-03 11:41:24 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'matthew-gernale/gernale-portfolio/'
+  base: '/gernale-portfolio/gh-pages'
 })
