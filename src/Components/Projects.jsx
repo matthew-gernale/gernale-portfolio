00fa980896d169ebd1af614c5b22a3ae0940a832
Matthew Gernale <dev.mattgernale@gmail.com>
2024-03-06 16:23:55 +0800

    Finished Project section

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -2,6 +2,25 @@ import '../index.scss'
 import Card from './Card'
 import BannerStroke from '../images/my-work.png'
 import BannerBlack from '../images/my-work-b.png'
+import JsIcon from '../images/js-icon.png'
+import CsharpIcon from '../images/csharp-icon.png'
+import HtmlIcon from '../images/html-icon.png'
+import CssIcon from '../images/css-icon.png'
+import ReactIcon from '../images/react-icon.png'
+import TailwindIcon from '../images/tailwind-icon.png'
+import BootstrapIcon from '../images/bootstrap-icon.png'
+import MssqlIcon from '../images/mssql-icon.png'
+import FigmaIcon from '../images/figma-icon.png'
+import PsIcon from '../images/ps-icon.png'
+import resort from '../images/jm-resort.png'
+import Ems from '../images/management-system.png'
+import Jm from '../images/jm-landing-page.pdf'
+import P2p from '../images/p2p-app.pdf'
+import P2pImg from '../images/p2p-app.png'
+import CityApp from '../images/city-gallery.png'
+import SnakeGame from '../images/snake-game.png'
+import NotedApp from '../images/noted-app.png'
+
 
 
 const Projects = () => {
@@ -13,12 +32,12 @@ const Projects = () => {
             </div>
             <h1 className="title text-6xl pb-20">Projects</h1>
             <div className="card-wrapper grid grid-flow-row grid-cols-3 gap-7">
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
+                <Card title="Employee Management System" imageUrl={Ems} icon={[CsharpIcon, HtmlIcon, CssIcon, BootstrapIcon, MssqlIcon]} project="https://youtu.be/_g9xKQa6P5w"/>
+                <Card title="Snake Game" imageUrl={SnakeGame} icon={[ReactIcon, HtmlIcon, CssIcon, JsIcon, TailwindIcon]} project="https://matthew-gernale.github.io/JavaSnake/" />
+                <Card title="Noted" imageUrl={NotedApp} icon={[ReactIcon, HtmlIcon, CssIcon, JsIcon, BootstrapIcon]} project="https://matthew-gernale.github.io/noted/" />
+                <Card title="City Gallery" imageUrl={CityApp} icon={[HtmlIcon, CssIcon]} project="https://matthew-gernale.github.io/city-gallery/" />
+                <Card title="Jm Resort Landing Page" imageUrl={resort} icon={[FigmaIcon, PsIcon]} project={Jm} />
+                <Card title="P2P Booking App" imageUrl={P2pImg} icon={[FigmaIcon]} project={P2p} />
             </div>
         </div>
     );
